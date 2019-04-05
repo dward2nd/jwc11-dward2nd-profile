b136f05a5507f4d66a2107d386b248d67b5fa701
Saksinkarn Petchkuljinda <dward.the.2nd@gmail.com>
2019-04-05 17:21:52 +0700

    general info + exp

---
+++ js/index.js
@@ -0,0 +1,20 @@
+var app = new Vue ({
+    el: '#main',
+    data: {
+        plats: [
+            {
+                name: 'facebook',
+                info: 'Saksinkarn Petchkuljinda',
+                link: 'https://www.facebook.com/dward2nd'
+            }, {
+                name: 'instagram',
+                info: 'dward2nd',
+                link: 'https://www.instagram.com/dward2nd'
+            }, {
+                name: 'github',
+                info: 'dward2nd',
+                link: 'https://github.com/dward2nd'
+            }
+        ]
+    }
+});
